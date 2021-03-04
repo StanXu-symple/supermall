bdaf9497e15510c1666c382120d2496954770bb8
StanXu <1316239136@qq.com>
2021-03-04 09:56:48 +0800

    通过axios访问数据并存给定义的变量域

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -13,10 +13,20 @@ import {getHomeMultidata} from "../../network/home";
 export default {
   name: "Home",
   components: {NavBar},
+  data(){
+    return {
+      //定义一个初始数据为空的banner域
+      banners: null
+    }
+  },
   created(){
     //1.请求多个数据
+    //这里的res即为通过axios访问服务器所返回的值,记住这里的返回值函数写法为.then(xxx=>{})
     getHomeMultidata().then(res=>{
       console.log(res);
+      //给banner域赋值，赋的值为前台访问服务器拿到的数据
+      this.banners=res.data.banner.list;
+      console.log(this.banners)
     })
   }
 }
